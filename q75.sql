e4eaff8aa5b23f8107bccb7b8b74ab7792b44403
jonathanpasuy641 <jonathan.pasuy641@pascualbravo.edu.co>
2026-09-23 18:27:52 -0500

    Delete q75.sql

--- q75.sql
+++
@@ -1,7 +0,0 @@
--- 7.5 Cantidad de gaseosas vendidas por producto y por región, de mayor a menor
-SELECT r.nombre AS region, vo.producto, SUM(vo.cantidad) AS cantidad_total
-FROM vista_operaciones vo
-JOIN operaciones op ON op.id_registro = vo.id_registro
-JOIN regiones r ON r.id_region = op.id_region
-GROUP BY r.nombre, vo.producto
-ORDER BY cantidad_total DESC;
