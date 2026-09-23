50b01bfd27ed131772e9af6f12d8f1fa034ee6dc
jonathanpasuy641 <jonathan.pasuy641@pascualbravo.edu.co>
2026-09-23 18:27:38 -0500

    Delete q73.sql

--- q73.sql
+++
@@ -1,7 +0,0 @@
--- 7.3 Top 5 departamentos con mayor cantidad de MANZALOCA vendida
-SELECT departamento, SUM(cantidad) AS cantidad_total
-FROM vista_operaciones
-WHERE producto = 'MANZALOCA'
-GROUP BY departamento
-ORDER BY cantidad_total DESC
-LIMIT 5;
