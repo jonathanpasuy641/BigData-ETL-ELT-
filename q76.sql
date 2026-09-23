7e3e9f775dd0df12768da3a8ec64548b1befb46a
jonathanpasuy641 <jonathan.pasuy641@pascualbravo.edu.co>
2026-09-23 18:27:58 -0500

    Delete q76.sql

--- q76.sql
+++
@@ -1,6 +0,0 @@
--- 7.6 Monto total de ventas de cada producto en Antioquia, de mayor a menor
-SELECT producto, SUM(venta) AS monto_total
-FROM vista_operaciones
-WHERE departamento = 'Antioquia'
-GROUP BY producto
-ORDER BY monto_total DESC;
