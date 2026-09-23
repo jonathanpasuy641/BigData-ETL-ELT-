dec11625a9e6991b8245df701f2136f0e0cc1f93
jonathanpasuy641 <jonathan.pasuy641@pascualbravo.edu.co>
2026-09-23 18:27:24 -0500

    Delete q71.sql

--- q71.sql
+++
@@ -1,6 +0,0 @@
--- 7.1 Top 8 departamentos con mayor VOLUMEN de ventas (monto), de mayor a menor
-SELECT departamento, SUM(venta) AS monto_total
-FROM vista_operaciones
-GROUP BY departamento
-ORDER BY monto_total DESC
-LIMIT 8;
